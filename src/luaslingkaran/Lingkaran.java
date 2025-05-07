5fedcd02a3b1e617f24121bae3d88be925ec9d2f
Abdul Kader <abdulkader0126@gmail.com>
2025-05-07 20:00:07 +0700

    crt : kalkulator luas lingkaran

---
+++ src/luaslingkaran/Lingkaran.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package luaslingkaran;
+
+/**
+ *
+ * @author LEGION
+ */
+public class Lingkaran {
+
+    final double PI = 3.141592;
+
+    void hitungLuas(double r) {
+        double luas = PI * r * r;
+        System.out.println("Hasil Luas lingkaran (Pecahan) : " + luas);
+        System.out.println("Hasil Luas lingkaran (bulan) : " + (int) luas);
+        System.out.println("Hasil Luas lingkaran (pembulatan) : " + Math.round(luas));
+    }
+}
---
+++ src/luaslingkaran/LingkaranBeraksi.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package luaslingkaran;
+
+/**
+ *
+ * @author LEGION
+ */
+public class LingkaranBeraksi {
+
+    public static void main(String[] args) {
+        Lingkaran ling = new Lingkaran();
+
+        double r = 11.78;
+        ling.hitungLuas(r);
+    }
+}
